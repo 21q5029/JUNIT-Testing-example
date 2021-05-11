8a17e02c2fc5f2e2923e25614580bccd7fc69794
21q5029 <21q5029@bztf.ch>
2021-05-11 21:21:46 +0200

    11.05.2021 with main

--- junit_demo/src/main/java/ch/bztf/App.java
+++ junit_demo/src/main/java/ch/bztf/App.java
@@ -8,6 +8,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Calculate plus = new Calculate();
+        Calculate minus = new Calculate();
+        Calculate mal = new Calculate();
+        Calculate durch = new Calculate();
+
+        System.out.println("Addition: " +plus.add(10, 6));
+        System.out.println("Subtraktion: " +minus.subtract(30, 25));
+        System.out.println("Muliplikation: " +mal.multiply(20, 2));
+        System.out.println("Division: " +durch.divide(10, 2));
+
     }
 }
